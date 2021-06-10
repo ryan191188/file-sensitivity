ae50a4a2b306d12250bbe693ea37c01efa50c0f3
Ryan Goh <ryan_goh129@hotmail.com>
2021-06-10 20:22:02 +0800

    broker url issue

--- fsesite/celery.py
+++ fsesite/celery.py
@@ -6,6 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fsesite.settings')
 
 app = Celery('fsesite')
 app.config_from_object('django.conf:settings')
+app.conf.BROKER_URL = "amqp://fsadmin:ryan191188@18.141.139.57:5672"
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 app.conf.beat_schedule = {
