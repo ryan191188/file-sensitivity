60da3eb7cf19a6ab3e8d207d97a816c63ed0ebe9
Ryan Goh <ryan_goh129@hotmail.com>
2021-06-10 21:18:48 +0800

    Revert "broker url issue"
    This reverts commit ae50a4a2b306d12250bbe693ea37c01efa50c0f3.

--- fsesite/celery.py
+++ fsesite/celery.py
@@ -6,7 +6,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fsesite.settings')
 
 app = Celery('fsesite')
 app.config_from_object('django.conf:settings')
-app.conf.BROKER_URL = "amqp://fsadmin:ryan191188@18.141.139.57:5672"
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 app.conf.beat_schedule = {
